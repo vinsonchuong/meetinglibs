fccdb5477a62a562a8035093907d14f72f7c6c55
Vinson Chuong <Vinson.Chuong@gmail.com>
2013-01-21 21:38:08 -0800

    Initialize Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetinglibs::Application.config.secret_token = '988930fce86f6ddc5a3bfeeffbd3cb4613d1883454eda870eaf5dbe430ec028290be264a2ecea2f4e9faac38b790b9aac3523a886d53964f6a68acaef02a6122'
