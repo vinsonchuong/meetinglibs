08d150e2f89f0cf7e1f93522d8943df6ec803ec0
Vinson Chuong <Vinson.Chuong@gmail.com>
2013-01-23 07:54:10 -0800

    Remove secret_token from repo (into /secret.rb).

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Source credentials and encryption keys
+secrets = Rails.root.join('secrets.rb')
+load(secrets) if File.exists?(secrets)
+
 # Initialize the rails application
 MeetingLibs::Application.initialize!
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MeetingLibs::Application.config.secret_token = '988930fce86f6ddc5a3bfeeffbd3cb4613d1883454eda870eaf5dbe430ec028290be264a2ecea2f4e9faac38b790b9aac3523a886d53964f6a68acaef02a6122'
+MeetingLibs::Application.config.secret_token = ENV['secret_token']
